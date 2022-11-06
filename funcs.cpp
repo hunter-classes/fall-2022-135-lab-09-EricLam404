11b26a5d52e09939eaa6854e317d808004a0802a
EricLam404 <lameric9888@gmail.com>
2022-11-06 10:00:45 -0500

    Added Task E

--- funcs.cpp
+++ funcs.cpp
@@ -17,3 +17,15 @@ void move(Coord3D *ppos, Coord3D *pvel, double dt){
     ppos->y += pvel->y * dt;
     ppos->z += pvel->z * dt;
 }
+
+Coord3D* createCoord3D(double x, double y, double z){
+    Coord3D *coord = new Coord3D();
+    coord->x = x;
+    coord->y = y;
+    coord->z = z;
+    return coord;
+}
+
+void deleteCoord3D(Coord3D *p){
+    delete p;
+}
--- main.cpp
+++ main.cpp
@@ -13,12 +13,29 @@ int main(){
 
     Coord3D * ans = fartherFromOrigin(&pointP, &pointQ);
    
-    std::cout << "ans = " << ans << std::endl;
+    std::cout << "ans = " << ans << std::endl << std::endl;
 
     Coord3D pos = {0, 0, 100.0};
     Coord3D vel = {1, -5, 0.2};
 
     move(&pos, &vel, 2.0); // object pos gets changed
-    std::cout << pos.x << " " << pos.y << " " << pos.z << std::endl;
+    std::cout << pos.x << " " << pos.y << " " << pos.z << std::endl << std::endl;
     // prints: 2 -10 100.4
+
+    double x, y, z;
+    std::cout << "Enter position: ";
+    std::cin >> x >> y >> z;
+    Coord3D *ppos = createCoord3D(x,y,z);
+    
+    std::cout << "Enter velocity: ";
+    std::cin >> x >> y >> z;
+    Coord3D *pvel = createCoord3D(x,y,z);
+
+    move(ppos, pvel, 10.0);
+
+    std::cout << "Coordinates after 10 seconds: " 
+         << (*ppos).x << " " << (*ppos).y << " " << (*ppos).z << std::endl;
+
+    deleteCoord3D(ppos); // release memory
+    deleteCoord3D(pvel);
 }
