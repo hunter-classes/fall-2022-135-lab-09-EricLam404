984e3a7fce97b454dd3b41a1261d3e82274b3b2d
EricLam404 <lameric9888@gmail.com>
2022-11-06 09:46:46 -0500

    Added Task B

--- funcs.cpp
+++ funcs.cpp
@@ -7,3 +7,7 @@
 double length(Coord3D *p){
     return sqrt(pow(p->x, 2) + pow(p->y, 2) + pow(p->z, 2));
 }
+
+Coord3D * fartherFromOrigin(Coord3D *p1, Coord3D *p2){
+    return length(p1) > length(p2) ? p1 : p2;
+}
--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 #include "funcs.h"
 
 int main(){
+    Coord3D point = {10, 20, 30};
+    std::cout << length(&point) << "\n";
+
     Coord3D pointP = {10, 20, 30};
-    std::cout << length(&pointP) << "\n";
+    Coord3D pointQ = {-20, 21, -22};
+
+    std::cout << "Address of P: " << &pointP << std::endl;
+    std::cout << "Address of Q: " << &pointQ << std::endl << std::endl;
+
+    Coord3D * ans = fartherFromOrigin(&pointP, &pointQ);
+   
+    std::cout << "ans = " << ans << std::endl;
 }
