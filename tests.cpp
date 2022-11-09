e5f74a99576640812256403bfbb9e10e051498b0
EricLam404 <lameric9888@gmail.com>
2022-11-08 21:58:02 -0500

    Added more tests

--- tests.cpp
+++ tests.cpp
@@ -5,5 +5,8 @@
 // add your tests here
 TEST_CASE("Task A"){
     Coord3D point1 = {10, 20, 30};
+    Coord3D point2 = {10, 20, 30};
+
     CHECK(length(&point1) == 37.4166);
+    CHECK(length(&point2) == 37.4166);
 }
